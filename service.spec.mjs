be9aa622fb42f8c1521dce8f2b0839d39ac2af25
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-12-29 08:34:59 -0600

    initial: wip

---
+++ service.spec.mjs
@@ -0,0 +1,8 @@
+import { ok } from 'node:assert'
+import { describe, it } from 'node:test'
+
+describe('service', () => {
+  it('should start openmct', () => {
+    ok(true, 'openmct started')
+  })
+})
